c4b5f387ddd32b202b2d06d84461fb7c2ba88797
Dipankar Adhikary <dipankaradhikary1994@gmail.com>
2025-08-02 14:27:54 +0530

    Complete CI/CD setup

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return jsonify(message="Hello from Flask app in Docker!")
+    return {"message": os.getenv("APP_MESSAGE", "Default message")}
 
-@app.route("/health")
+@app.route('/health')
 def health():
-    return jsonify(status="healthy")
+    return {"status": "healthy", "env": os.getenv("FLASK_ENV")}
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
